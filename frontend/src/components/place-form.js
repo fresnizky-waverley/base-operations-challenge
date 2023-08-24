443937624f411b6e900c01d617ddf3643173f3e4
Federico Resnizky <fresnizky@waverleysoftware.com>
2023-08-24 11:04:18 -0300

    Added add place form

--- frontend/src/components/place-form.js
+++ frontend/src/components/place-form.js
@@ -1,16 +1,19 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import AutoComplete from "react-google-autocomplete";
 
-export default function PlaceSearch({ handlePlaceSearch }) {
+export default function PlaceForm({ handlePlaceAdd }) {
+  const [place, setPlace] = useState(null);
+  const nameRef = useRef(null);
   const latRef = useRef(null);
   const lngRef = useRef(null);
-  const radiusRef = useRef(null);
 
   return (
     <div className="">
+      <input name="name" placeholder="Name" ref={nameRef} />
       <AutoComplete
         apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
         onPlaceSelected={(place) => {
+          setPlace(place);
           latRef.current.value = place.geometry.location.lat();
           lngRef.current.value = place.geometry.location.lng();
         }}
@@ -18,24 +21,23 @@ export default function PlaceSearch({ handlePlaceSearch }) {
           types: [],
           componentRestrictions: { country: "us" },
         }}
-        placeholder="Search location"
+        placeholder="Add location"
       />
 
       <input name="lat" placeholder="Latitude" ref={latRef} />
       <input name="lng" placeholder="Longitude" ref={lngRef} />
 
-      <input type="number" placeholder="Radius" name="radius" ref={radiusRef} />
-
       <button
         onClick={() =>
-          handlePlaceSearch({
+          handlePlaceAdd({
+            name: nameRef.current.value,
+            address: place.formatted_address,
             lat: latRef.current.value,
             lng: lngRef.current.value,
-            radius: radiusRef.current.value * 1000,
           })
         }
       >
-        Search
+        Add
       </button>
     </div>
   );
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import Head from 'next/head'
 import Layout from '../components/layout';
 import PlaceSearch from '../components/place-search';
+import PlaceForm from '../components/place-form'
+
+type PlaceAddParams = {
+  name: string,
+  address: string
+  lat: number,
+  lng: number,
+}
 
 type PlaceSearchParams = {
   lat: number,
@@ -11,6 +19,32 @@ type PlaceSearchParams = {
 
 export default function Home() {
   const [places, setPlaces] = useState([]);
+
+  const handlePlaceAdd = ({name, address, lat, lng}: PlaceAddParams) => {
+    if (!lat || !lng) {
+      return;
+    }
+
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/place`, {
+      method: 'POST',
+      body: JSON.stringify({
+        name: name,
+        address: address,
+        latitude: lat,
+        longitude: lng
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setPlaces(data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
 
   const handlePlaceSearch = ({lat, lng, radius}: PlaceSearchParams) => {
     if (!lat || !lng || !radius) {
@@ -35,6 +69,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
+        <PlaceForm handlePlaceAdd={handlePlaceAdd} />
         <PlaceSearch handlePlaceSearch={handlePlaceSearch} />
       </Layout>
     </div>
